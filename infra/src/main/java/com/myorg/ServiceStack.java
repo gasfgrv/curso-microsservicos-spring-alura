d03880cee9698274b3fb606aeb3459a729eb0a47
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-04-22 01:32:28 -0300

    Configuração do LogGroup

--- infra/src/main/java/com/myorg/ServiceStack.java
+++ infra/src/main/java/com/myorg/ServiceStack.java
@@ -4,15 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 import software.amazon.awscdk.Fn;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ecr.IRepository;
 import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
 public class ServiceStack extends Stack {
@@ -34,10 +38,10 @@ public class ServiceStack extends Stack {
 
 		IRepository repositorio = Repository.fromRepositoryName(this, "repositorio", "pedidos-ms");
 
-		service = ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+		service = ApplicationLoadBalancedFargateService.Builder.create(this,  "AluraService")
 				.cluster(cluster)
 				.cpu(512)
-				.desiredCount(1)
+				.desiredCount(3)
 				.listenerPort(8080)
 				.assignPublicIp(true)
 				.taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
@@ -45,6 +49,12 @@ public class ServiceStack extends Stack {
 						.containerPort(8080)
 						.containerName("pedidos")
 						.environment(autenticacao)
+						.logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+								.logGroup(LogGroup.Builder.create(this, "PedidosMsLog")
+										.removalPolicy(RemovalPolicy.DESTROY)
+										.build())
+								.streamPrefix("PedidosMs")
+								.build()))
 						.build())
 				.memoryLimitMiB(2048)
 				.publicLoadBalancer(true)
