2bbd855e594353016526c6e1d09392c5914a9a92
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-08 23:59:37 -0300

    configuração para acessar o rabbitmq

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.alurafood.pagamentos.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PagamentoAMQPConfiguration {
+
+    @Bean
+    public Queue criarFila() {
+        return new Queue("pagamento.confirmado", false);
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory factory) {
+        return new RabbitAdmin(factory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+}
