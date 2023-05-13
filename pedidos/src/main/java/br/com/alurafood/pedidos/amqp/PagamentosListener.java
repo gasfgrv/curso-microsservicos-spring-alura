ce64f1898cafc5685e4426544e61af4c9ebd06fb
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-12 23:14:04 -0300

    Recebendo mensagems da exchange

--- pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentosListener.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentosListener.java
@@ -2,7 +2,6 @@ package br.com.alurafood.pedidos.amqp;
 
 import br.com.alurafood.pedidos.dto.PagamentoDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PagamentosListener {
 
-    @RabbitListener(queues = "pagamento.confirmado")
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
     public void lerMensagem(PagamentoDto pagamento) {
         log.info("Recebi a mensagem: {}", pagamento);
     }
