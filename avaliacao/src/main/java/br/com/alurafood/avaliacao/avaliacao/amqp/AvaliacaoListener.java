a2b83135c31768183c4bdc2a8914295351fc85ce
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-05-12 23:40:36 -0300

    Retentativas em caso de falha

--- avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
+++ avaliacao/src/main/java/br/com/alurafood/avaliacao/avaliacao/amqp/AvaliacaoListener.java
@@ -1,6 +1,7 @@
 package br.com.alurafood.avaliacao.avaliacao.amqp;
 
 import br.com.alurafood.avaliacao.avaliacao.dto.PagamentoDto;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -12,6 +13,9 @@ public class AvaliacaoListener {
 
     @RabbitListener(queues = "pagamentos.detalhes-avaliacao")
     public void receberMensagem(@Payload PagamentoDto dto) {
+
+        if (Objects.equals(dto.getCodigo(), "000")) throw new RuntimeException("Código inválido");
+
         var mensagem = """
                 Necessário criar registro de avaliação para o pedido: {}
                 Id do pagamento: {}
